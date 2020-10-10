72d4e0325510cc27cc459efcaf56a04bc07cc3f4
Naker90 <Antoniojesussg96@gmail.com>
2020-10-10 20:06:29 +0200

    feat(OpenApi): configure Swashbuckle nuget package to serve the api specification

--- src/WebApp/backend/WebAppApi/Startup.cs
+++ src/WebApp/backend/WebAppApi/Startup.cs
@@ -26,6 +26,7 @@ namespace WebAppApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +35,13 @@ namespace WebAppApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                app.UseSwagger();
+                app.UseSwaggerUI(c => 
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Envios Canarios Web App Api");
+                    c.RoutePrefix = "_doc";
+                });
             }
 
             app.UseHttpsRedirection();
