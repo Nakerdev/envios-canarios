3bee650758d798a7b0e8c40bb0ee5f0b46b67417
Naker90 <Antoniojesussg96@gmail.com>
2020-10-11 19:52:36 +0200

    featr(WebAppApi): Created the bad request response model

---
+++ src/WebApp/backend/WebAppApi/Utils/BadRequestResponseModel.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+
+namespace WebAppApi.Utils
+{
+    public sealed class BadRequestResponseModel<ValidationErrorCode> where ValidationErrorCode : Enum
+    {
+        public List<ValidationError<ValidationErrorCode>> ValidationErrors { get; }
+        public string OperationError { get; }
+
+        public static BadRequestResponseModel<ValidationErrorCode> CreateValidationErrorResponse(
+            List<ValidationError<ValidationErrorCode>> validationErrors)
+        {
+            return new BadRequestResponseModel<ValidationErrorCode>(
+                validationErrors: validationErrors,
+                operationError: null);
+        }
+
+        public static BadRequestResponseModel<ValidationErrorCode> CreateOperationErrorResponse(
+            string operationError)
+        {
+            return new BadRequestResponseModel<ValidationErrorCode>(
+                validationErrors: null,
+                operationError: operationError);
+        }
+
+        private BadRequestResponseModel(
+            List<ValidationError<ValidationErrorCode>> validationErrors, 
+            string operationError)
+        {
+            this.ValidationErrors = validationErrors;
+            this.OperationError = operationError;
+        }
+    }
+
+    public sealed class ValidationError<ValidationErrorCode> where ValidationErrorCode : Enum
+    {
+        public string FieldId { get; }
+        public string ErrorCode { get; }
+
+        public ValidationError(
+            string fieldId, 
+            ValidationErrorCode errorCode)
+        {
+            this.FieldId = fieldId;
+            this.ErrorCode = errorCode.ToString();
+        }
+    }
+}
