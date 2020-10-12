3c720b7d80d458b421575fdd3037aab2824dc39e
Naker90 <Antoniojesussg96@gmail.com>
2020-10-12 11:56:50 +0200

    feat(WebAppApi): extract class to another file

--- src/WebApp/backend/WebAppApi/PurchaseApplication/Controllers/PurchaseApplicationController.cs
+++ src/WebApp/backend/WebAppApi/PurchaseApplication/Controllers/PurchaseApplicationController.cs
@@ -78,30 +78,4 @@ namespace WebAppApi.PurchaseApplication.Controllers
         WrongLength,
         InvalidValue
     }
-
-    public sealed class PurchaseApplicationRequestExample : IExamplesProvider<PurchaseApplicationController.PurchaseApplicationRequest>
-    {
-        public PurchaseApplicationController.PurchaseApplicationRequest GetExamples()
-        {
-            return new PurchaseApplicationController.PurchaseApplicationRequest
-            {
-                Products = new List<PurchaseApplicationController.Product>
-                {
-                    new PurchaseApplicationController.Product
-                    {
-                        Link = "https://www.adidas.es/zapatilla-zx-2k-4d/FY9089.html",
-                        Units = "1",
-                        AdditionalInformation = "Color negro, talla 43",
-                        PromotionCode = "ADDIDAS-123"
-                    }
-                },
-                Client = new PurchaseApplicationController.Client{
-                    Name = "Alfredo",
-                    TelephoneNumber = "123123123",
-                    Email = "alfredo@elguapo.com"
-                },
-                AdditionalInformation = "Si el código promocional no funciona quiero el producto igualmente"
-            };
-        }
-    }
 }
