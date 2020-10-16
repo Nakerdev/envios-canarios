4d6f9484e909c2c3038649b9714d5bd76c0635c2
Naker90 <Antoniojesussg96@gmail.com>
2020-10-16 21:49:36 +0200

    feat(WebAppApi): partial commits: unit tests

--- src/Tests/Unit/WebAppApi/PurchaseApplication/Controllers/PurchaseApplicationControllerTests.cs
+++ src/Tests/Unit/WebAppApi/PurchaseApplication/Controllers/PurchaseApplicationControllerTests.cs
@@ -1,10 +1,12 @@
+using System.ComponentModel;
 using NUnit.Framework;
+using WebAppApi.PurchaseApplication.Controllers;
 
 namespace EnviosCanarios.Tests.Unit.WebAppApi.PurchaseApplication.Controllers
 {
     public class PurchaseApplicationControllerTests
     {
-        private readonly PurchaseApplicationController controller;
+        private PurchaseApplicationController controller;
 
         [SetUp]
         public void Setup()
@@ -13,9 +15,32 @@ namespace EnviosCanarios.Tests.Unit.WebAppApi.PurchaseApplication.Controllers
         }
 
         [Test]
-        public void Test1()
+        public void CreatesPurchaseApplication()
         {
-            Assert.Pass();
+            var request = new PurchaseApplicationController.PurchaseApplicationRequest
+            {
+                Products = new List<PurchaseApplicationController.Product>
+                {
+                    new PurchaseApplicationController.Product
+                    {
+                        Link = "https://www.addida.com/any/product",
+                        Units = "1",
+                        AdditionalInformation = "Additional product information",
+                        PromotionCode = "ADDIDAS-123"
+                    }
+                },
+                Client = new  PurchaseApplicationController.Client
+                {
+                    Name = "Alfredo",
+                    Phone = "123123123",
+                    Email = "alfredo@elguapo.com"
+                },
+                AdditionalInformation = "Additional purchase application information"
+            };
+
+            var response = controller.Execute(request);
+
+            false.Should().BeTrue();
         }
     }
 }
