d26ca4ac13682b9ad496fad440e353c995313393
Antonio Sánchez <antoniojesussg96@gmail.com>
2020-11-02 11:15:58 +0100

    delete duplicated tests

--- src/Tests/Unit/Domain/PurchaseApplication/ValueObjects/LinkTests.cs
+++ src/Tests/Unit/Domain/PurchaseApplication/ValueObjects/LinkTests.cs
@@ -22,52 +22,6 @@ namespace CanaryDeliveries.Tests.Domain.PurchaseApplication.ValueObjects
             result.IsSuccess.Should().BeTrue();
         }
 
-        [Test]
-        public void DoesNotCreateALinkWhenValueIsEmpty()
-        {
-            var result = Link.Create(None);
-
-            result.IsFail.Should().BeTrue();
-            result.IfFail(validationErrors =>
-            {
-                validationErrors.Count.Should().Be(1);
-                validationErrors.First().FieldId.Should().Be(nameof(Link));
-                validationErrors.First().ErrorCode.Should().Be(LinkValidationErrorCode.Required);
-            });
-        }
-        
-        [Test]
-        public void DoesNotCreateALinkWhenValueIsTooLong()
-        {
-            var link = new string('a', 1001);
-            
-            var result = Link.Create(link);
-
-            result.IsFail.Should().BeTrue();
-            result.IfFail(validationErrors =>
-            {
-                validationErrors.Count.Should().Be(1);
-                validationErrors.First().FieldId.Should().Be(nameof(Link));
-                validationErrors.First().ErrorCode.Should().Be(LinkValidationErrorCode.WrongLength);
-            });
-        }
-        
-        [Test]
-        public void DoesNotCreateALinkWhenValueIsNotAValidLinkFormat()
-        {
-            const string link = "https//not-valid-link";
-            
-            var result = Link.Create(link);
-
-            result.IsFail.Should().BeTrue();
-            result.IfFail(validationErrors =>
-            {
-                validationErrors.Count.Should().Be(1);
-                validationErrors.First().FieldId.Should().Be(nameof(Link));
-                validationErrors.First().ErrorCode.Should().Be(LinkValidationErrorCode.InvalidFormat);
-            });
-        }
-
         public class ValidationErrorTestCase<IValidationErrorCode, IType>
         {
             public Func<Validation<ValidationError<IValidationErrorCode>, IType>> Builder { get; }
