96173e145b08e9091b71d1b814ddfca94d4b8b72
Antonio Sánchez <antoniojesussg96@gmail.com>
2020-11-02 11:15:17 +0100

    created validation error tests parametrized tests

--- src/Tests/Unit/Domain/PurchaseApplication/ValueObjects/LinkTests.cs
+++ src/Tests/Unit/Domain/PurchaseApplication/ValueObjects/LinkTests.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Collections.Generic;
+using CanaryDeliveries.Domain.PurchaseApplication.Create;
 using CanaryDeliveries.Domain.PurchaseApplication.ValueObjects;
 using FluentAssertions;
+using LanguageExt;
 using NUnit.Framework;
 using static LanguageExt.Prelude;
 
@@ -17,7 +21,7 @@ namespace CanaryDeliveries.Tests.Domain.PurchaseApplication.ValueObjects
 
             result.IsSuccess.Should().BeTrue();
         }
-        
+
         [Test]
         public void DoesNotCreateALinkWhenValueIsEmpty()
         {
@@ -62,6 +66,57 @@ namespace CanaryDeliveries.Tests.Domain.PurchaseApplication.ValueObjects
                 validationErrors.First().FieldId.Should().Be(nameof(Link));
                 validationErrors.First().ErrorCode.Should().Be(LinkValidationErrorCode.InvalidFormat);
             });
+        }
+
+        public class ValidationErrorTestCase<IValidationErrorCode, IType>
+        {
+            public Func<Validation<ValidationError<IValidationErrorCode>, IType>> Builder { get; }
+            public string ExpectedFieldId { get; }
+            public IValidationErrorCode ExpectedErrorCode { get; }
+
+            public ValidationErrorTestCase(
+                Func<Validation<ValidationError<IValidationErrorCode>, IType>> builder, 
+                string expectedFieldId, 
+                IValidationErrorCode expectedErrorCode)
+            {
+                Builder = builder;
+                ExpectedFieldId = expectedFieldId;
+                ExpectedErrorCode = expectedErrorCode;
+            }
+        }
+        
+        private readonly IReadOnlyList<ValidationErrorTestCase<LinkValidationErrorCode, Link>> _validationErrorTestCases =
+            new List<ValidationErrorTestCase<LinkValidationErrorCode, Link>>
+            {
+                new ValidationErrorTestCase<LinkValidationErrorCode, Link>(
+                    builder: () => Link.Create(None),
+                    expectedFieldId: nameof(Link),
+                    expectedErrorCode: LinkValidationErrorCode.Required),
+                new ValidationErrorTestCase<LinkValidationErrorCode, Link>(
+                    builder: () => Link.Create(new string('a', 1001)),
+                    expectedFieldId: nameof(Link),
+                    expectedErrorCode: LinkValidationErrorCode.WrongLength),
+                new ValidationErrorTestCase<LinkValidationErrorCode, Link>(
+                    builder: () => Link.Create("https//not-valid-link"),
+                    expectedFieldId: nameof(Link),
+                    expectedErrorCode: LinkValidationErrorCode.InvalidFormat)
+            }.AsReadOnly();
+
+        [Test]
+        public void DoesNotCreateALinkWhenThereIsValidationError()
+        {
+            foreach (var validationErrorTestCase in _validationErrorTestCases)
+            {
+                var result = validationErrorTestCase.Builder();
+            
+                result.IsFail.Should().BeTrue();
+                result.IfFail(validationErrors =>
+                {
+                    validationErrors.Count.Should().Be(1);
+                    validationErrors.First().FieldId.Should().Be(validationErrorTestCase.ExpectedFieldId);
+                    validationErrors.First().ErrorCode.Should().Be(validationErrorTestCase.ExpectedErrorCode);
+                });    
+            }
         }
     }
 }
