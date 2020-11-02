d1bfea467aecc0fc9371a2650fc6a8379cdabc10
Antonio Sánchez <antoniojesussg96@gmail.com>
2020-11-02 10:53:30 +0100

    #refactor extract method

--- src/Domain/PurchaseApplication/ValueObjects/Link.cs
+++ src/Domain/PurchaseApplication/ValueObjects/Link.cs
@@ -8,8 +8,7 @@ namespace CanaryDeliveries.Domain.PurchaseApplication.ValueObjects
     {
         private string value;
         
-        public static Validation<ValidationError<LinkValidationErrorCode>, Link> Create(
-            Option<string> value)
+        public static Validation<ValidationError<LinkValidationErrorCode>, Link> Create(Option<string> value)
         {
             return
                 from link in ValidateRequire(value)
@@ -21,9 +20,7 @@ namespace CanaryDeliveries.Domain.PurchaseApplication.ValueObjects
             {
                 return val
                     .Map(v => new Link(v))
-                    .ToValidation(new ValidationError<LinkValidationErrorCode>(
-                        fieldId: nameof(Link),
-                        errorCode: LinkValidationErrorCode.Required));
+                    .ToValidation(CreateValidationError(LinkValidationErrorCode.Required));
             }
             
             Validation<ValidationError<LinkValidationErrorCode>, Link> ValidateLenght(Link link)
@@ -31,9 +28,7 @@ namespace CanaryDeliveries.Domain.PurchaseApplication.ValueObjects
                 const int maxAllowedLenght = 1000;
                 if (link.value.Length > maxAllowedLenght)
                 {
-                    return new ValidationError<LinkValidationErrorCode>(
-                        fieldId: nameof(Link),
-                        errorCode: LinkValidationErrorCode.WrongLength);
+                    return CreateValidationError(LinkValidationErrorCode.WrongLength);
                 }
                 return link;
             }
@@ -42,11 +37,14 @@ namespace CanaryDeliveries.Domain.PurchaseApplication.ValueObjects
             {
                 if (!Uri.IsWellFormedUriString(link.value, UriKind.Absolute))
                 {
-                    return new ValidationError<LinkValidationErrorCode>(
-                        fieldId: nameof(Link),
-                        errorCode: LinkValidationErrorCode.InvalidFormat);
+                    return CreateValidationError(LinkValidationErrorCode.InvalidFormat);
                 }
                 return link;
+            }
+
+            ValidationError<LinkValidationErrorCode> CreateValidationError(LinkValidationErrorCode errorCode)
+            {
+                return new ValidationError<LinkValidationErrorCode>(fieldId: nameof(Link), errorCode: errorCode);
             }
         }
 
