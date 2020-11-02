d9bd3228b7884115a430ceb6a1ca6359b2c50cf7
Antonio Sánchez <antoniojesussg96@gmail.com>
2020-11-02 10:05:20 +0100

    green DoesNotCreateALinkWhenValueIsEmpty

--- src/Tests/Unit/Domain/PurchaseApplication/ValueObjects/LinkTests.cs
+++ src/Tests/Unit/Domain/PurchaseApplication/ValueObjects/LinkTests.cs
@@ -1,6 +1,7 @@
 using CanaryDeliveries.Domain.PurchaseApplication.ValueObjects;
 using FluentAssertions;
 using NUnit.Framework;
+using static LanguageExt.Prelude;
 
 namespace CanaryDeliveries.Tests.Domain.PurchaseApplication.ValueObjects
 {
@@ -19,6 +20,20 @@ namespace CanaryDeliveries.Tests.Domain.PurchaseApplication.ValueObjects
             {
                 var expectedLink = new Link(link);
                 createdLink.Should().Be(expectedLink);
+            });
+        }
+        
+        [Test]
+        public void DoesNotCreateALinkWhenValueIsEmpty()
+        {
+            var result = Link.Create(None);
+
+            result.IsFail.Should().BeTrue();
+            result.IfFail(validationErrors =>
+            {
+                validationErrors.Count.Should().Be(1);
+                validationErrors.First().FieldId.Should().Be(nameof(Link));
+                validationErrors.First().ErrorCode.Should().Be(LinkValidationError.Required);
             });
         }
     }
