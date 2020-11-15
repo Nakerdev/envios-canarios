2023b911dfa3d17e4928d7ad81ce43f1b6f1d434
Antonio Sánchez <antoniojesussg96@gmail.com>
2020-11-15 20:47:31 +0100

    created EF core configuration and DbContext project with the PurchaseApplication, Client and Product tables models

---
+++ src/PurchaseApplication/DbContext/PurchaseApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CanaryDeliveries.PurchaseApplication.DbContext
+{
+    public sealed class PurchaseApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        //public DbSet<PurchaseApplication> PurchaseApplications { get; set; } 
+        public DbSet<Client> Clients { get; set; } 
+        //public DbSet<Product> Products { get; set; } 
+        
+        private readonly string connectionString;
+
+        public PurchaseApplicationDbContext(string connectionString) : base()
+        {
+            this.connectionString = connectionString;
+        }
+
+        public PurchaseApplicationDbContext() : base()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
