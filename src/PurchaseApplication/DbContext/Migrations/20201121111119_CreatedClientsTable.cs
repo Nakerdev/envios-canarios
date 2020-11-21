9215cd9163a45312773dbc59a6bf5e026c88f2cd
Antonio Sánchez <antoniojesussg96@gmail.com>
2020-11-21 12:12:01 +0100

    #migration CreateClientTable

---
+++ src/PurchaseApplication/DbContext/Migrations/20201121111119_CreatedClientsTable.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CanaryDeliveries.PurchaseApplication.DbContext.Migrations
+{
+    public partial class CreatedClientsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(maxLength: 255, nullable: false),
+                    PhoneNumber = table.Column<string>(maxLength: 16, nullable: false),
+                    Email = table.Column<string>(maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
