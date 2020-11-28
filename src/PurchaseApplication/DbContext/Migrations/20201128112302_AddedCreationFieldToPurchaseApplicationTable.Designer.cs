d9915d3c1aafc0337272a35db1ba5b32f535dc51
Antonio Sánchez <antoniojesussg96@gmail.com>
2020-11-28 12:23:53 +0100

    migration: AddedCreationFieldToPurchaseApplicationTable

--- src/PurchaseApplication/DbContext/Migrations/20201128112302_AddedCreationFieldToPurchaseApplicationTable.Designer.cs
+++ src/PurchaseApplication/DbContext/Migrations/20201128112302_AddedCreationFieldToPurchaseApplicationTable.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using CanaryDeliveries.PurchaseApplication.DbContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace CanaryDeliveries.PurchaseApplication.DbContext.Migrations
 {
     [DbContext(typeof(PurchaseApplicationDbContext))]
-    partial class PurchaseApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201128112302_AddedCreationFieldToPurchaseApplicationTable")]
+    partial class AddedCreationFieldToPurchaseApplicationTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
