2056be0295d2c1b11b69e411c2bd45238383746c
Antonio Sánchez <antoniojesussg96@gmail.com>
2020-11-28 11:41:29 +0100

    added migrations execution to docker pipeline

---
+++ src/Databases/MigrationManager/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CanaryDeliveries.PurchaseApplication.DbContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace CanaryDeliveries.Databases.MigrationManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Applying Purchase Applications DB Migrations..");
+            using var dbContext = new PurchaseApplicationDbContext();
+            dbContext.Database.Migrate();
+            Console.WriteLine("Migrations applied.");
+        }
+    }
+}
