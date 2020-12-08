9ad76b0eb6042034ee18cac6f2ce8507d7e895f2
Antonio Sánchez <antoniojesussg96@gmail.com>
2020-12-08 14:03:17 +0100

    change login service implementation

--- src/Backoffice/backend/Api/Users/Login/Services/LoginService.cs
+++ src/Backoffice/backend/Api/Users/Login/Services/LoginService.cs
@@ -1,5 +1,8 @@
+using CanaryDeliveries.Backoffice.Api.Auth;
 using CanaryDeliveries.Backoffice.Api.Security;
 using CanaryDeliveries.Backoffice.Api.Users.Login.Repositories;
+using LanguageExt;
+using static LanguageExt.Prelude;
 
 namespace CanaryDeliveries.Backoffice.Api.Users.Login.Services
 {
@@ -7,26 +10,48 @@ namespace CanaryDeliveries.Backoffice.Api.Users.Login.Services
     {
         private readonly BackofficeUserRepository backofficeUserRepository;
         private readonly CryptoServiceProvider cryptoServiceProvider;
+        private readonly TokenHandler tokenHandler;
 
         public LoginService(
             BackofficeUserRepository backofficeUserRepository,
-            CryptoServiceProvider cryptoServiceProvider)
+            CryptoServiceProvider cryptoServiceProvider, 
+            TokenHandler tokenHandler)
         {
             this.backofficeUserRepository = backofficeUserRepository;
             this.cryptoServiceProvider = cryptoServiceProvider;
+            this.tokenHandler = tokenHandler;
         }
 
-        public virtual bool AreValidCredentials(LoginRequest request)
+        public virtual Either<LoginError, Token> Authenticate(LoginRequest request)
         {
-            return backofficeUserRepository
-                .SearchBy(request.Email)
-                .Match(
-                    None: () => false,
-                    Some: user =>
-                    {
-                        var passwordIntentHash = cryptoServiceProvider.ComputeHash(request.Password);
-                        return passwordIntentHash.Value == user.Password;
-                    });
+            return
+                from backofficeUser in SearchBackofficeUser(request.Email)
+                from _ in ValidateCredentials(backofficeUser, request.Password)
+                from token in CreateToken(backofficeUser)
+                select token;
+            
+            Either<LoginError, BackofficeUser> SearchBackofficeUser(string email)
+            {
+                return backofficeUserRepository.SearchBy(email)
+                    .ToEither(() => LoginError.UserNotFound);
+            }
+            
+            Either<LoginError, Unit> ValidateCredentials(
+                BackofficeUser user, 
+                string passwordIntent)
+            {
+                var passwordIntentHash = cryptoServiceProvider.ComputeHash(passwordIntent);
+                if(passwordIntentHash.Value == user.Password)
+                {
+                    return unit;
+                }
+                return LoginError.InvalidCredentials;
+            }
+            
+            Either<LoginError, Token> CreateToken(BackofficeUser user)
+            {
+                return tokenHandler.Create(user);
+            }
         }
         
         public sealed class LoginRequest
@@ -40,5 +65,11 @@ namespace CanaryDeliveries.Backoffice.Api.Users.Login.Services
                 Password = password;
             }
         }
+    }
+
+    public enum LoginError
+    {
+        UserNotFound,
+        InvalidCredentials
     }
 }
--- src/Tests/Backoffice/Unit/Api/Users/Login/Services/LoginServiceTests.cs
+++ src/Tests/Backoffice/Unit/Api/Users/Login/Services/LoginServiceTests.cs
@@ -1,4 +1,5 @@
 using System;
+using CanaryDeliveries.Backoffice.Api.Auth;
 using CanaryDeliveries.Backoffice.Api.Security;
 using CanaryDeliveries.Backoffice.Api.Users.Login.Repositories;
 using CanaryDeliveries.Backoffice.Api.Users.Login.Services;
@@ -13,6 +14,7 @@ namespace CanaryDeliveries.Tests.Backoffice.Unit.Api.Users.Login.Services
     {
         private Mock<BackofficeUserRepository> backofficeUserRepository;
         private Mock<CryptoServiceProvider> cryptoServiceProvider;
+        private Mock<TokenHandler> tokenHandler;
         private LoginService loginService;
 
         [SetUp]
@@ -20,13 +22,15 @@ namespace CanaryDeliveries.Tests.Backoffice.Unit.Api.Users.Login.Services
         {
             backofficeUserRepository = new Mock<BackofficeUserRepository>();
             cryptoServiceProvider = new Mock<CryptoServiceProvider>();
+            tokenHandler = new Mock<TokenHandler>();
             loginService = new LoginService(
                 backofficeUserRepository: backofficeUserRepository.Object,
-                cryptoServiceProvider: cryptoServiceProvider.Object);
+                cryptoServiceProvider: cryptoServiceProvider.Object, 
+                tokenHandler: tokenHandler.Object);
         }
         
         [Test]
-        public void ValidatesCredentials()
+        public void AuthenticatesUser()
         {
             var request = BuildRequest();
             var backofficeUser = BuildBackofficeUser(request.Email);
@@ -36,22 +40,47 @@ namespace CanaryDeliveries.Tests.Backoffice.Unit.Api.Users.Login.Services
            cryptoServiceProvider
                .Setup(x => x.ComputeHash(request.Password))
                .Returns(new Hash(value: backofficeUser.Password));
+           var token = new Token(value: "jwt-token");
+           tokenHandler
+               .Setup(x => x.Create(backofficeUser))
+               .Returns(token);
             
-            var areValidCredentials = loginService.AreValidCredentials(request);
-
-            areValidCredentials.Should().BeTrue();
+            var result = loginService.Authenticate(request);
+            
+            result.IsRight.Should().BeTrue();
         }
         
         [Test]
-        public void DoesNotValidateCredentialsWhenUserNotFound()
+        public void DoesNotAuthenticateUserWhenCredentialsAreNotValid()
+        {
+            backofficeUserRepository
+                .Setup(x => x.SearchBy(It.IsAny<string>()))
+                .Returns(BuildBackofficeUser());
+            cryptoServiceProvider
+               .Setup(x => x.ComputeHash(It.IsAny<string>()))
+               .Returns(new Hash(value: "another-password-hash"));
+            
+            var result = loginService.Authenticate(BuildRequest());
+
+            result.IsLeft.Should().BeTrue();
+            result.IfLeft(error => error.Should().Be(LoginError.InvalidCredentials));
+            tokenHandler
+                .Verify(x => x.Create(It.IsAny<BackofficeUser>()), Times.Never);
+        }
+        
+        [Test]
+        public void DoesNotAuthenticateUserWhenUserNotFound()
         {
             backofficeUserRepository
                 .Setup(x => x.SearchBy(It.IsAny<string>()))
                 .Returns((BackofficeUser) null);
             
-            var areValidCredentials = loginService.AreValidCredentials(BuildRequest());
+            var result = loginService.Authenticate(BuildRequest());
 
-            areValidCredentials.Should().BeFalse();
+            result.IsLeft.Should().BeTrue();
+            result.IfLeft(error => error.Should().Be(LoginError.UserNotFound));
+            tokenHandler
+                .Verify(x => x.Create(It.IsAny<BackofficeUser>()), Times.Never);
         }
         
         private static LoginService.LoginRequest BuildRequest()
@@ -61,7 +90,7 @@ namespace CanaryDeliveries.Tests.Backoffice.Unit.Api.Users.Login.Services
                 password: "userPassword");
         }
 
-        private static BackofficeUser BuildBackofficeUser(string userEmail)
+        private static BackofficeUser BuildBackofficeUser(string userEmail = "user@email.com")
         {
             return new BackofficeUser(
                 id: new Guid("9bd0af33-87b5-48d6-92a7-7122f71585a5"),
