e47e6ea04c1df5ad05120335aa96174806243869
Antonio Sánchez <antoniojesussg96@gmail.com>
2020-12-08 12:17:15 +0100

    extract methods

--- src/Tests/Backoffice/Unit/Api/Users/Login/Services/LoginServiceTests.cs
+++ src/Tests/Backoffice/Unit/Api/Users/Login/Services/LoginServiceTests.cs
@@ -10,21 +10,25 @@ namespace CanaryDeliveries.Tests.Backoffice.Unit.Api.Users.Login.Services
     [TestFixture]
     public sealed class LoginServiceTests
     {
+        private Mock<BackofficeUserRepository> backofficeUserRepository;
+        private Mock<CryptoServiceProvider> cryptoServiceProvider;
+        private LoginService loginService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            backofficeUserRepository = new Mock<BackofficeUserRepository>();
+            cryptoServiceProvider = new Mock<CryptoServiceProvider>();
+            loginService = new LoginService(
+                backofficeUserRepository: backofficeUserRepository.Object,
+                cryptoServiceProvider: cryptoServiceProvider.Object);
+        }
+        
         [Test]
         public void ValidatesCredentials()
         {
-            var backofficeUserRepository = new Mock<BackofficeUserRepository>();
-            var cryptoServiceProvider = new Mock<CryptoServiceProvider>();
-            var loginService = new LoginService(
-                backofficeUserRepository: backofficeUserRepository.Object,
-                cryptoServiceProvider: cryptoServiceProvider.Object);
-            var request = new LoginService.LoginRequest(
-                email: "user@email.com",
-                password: "userPassword");
-            var backofficeUser = new BackofficeUser(
-                id: new Guid("9bd0af33-87b5-48d6-92a7-7122f71585a5"),
-                email: request.Email,
-                password: "hashedPassword");
+            var request = BuildRequest();
+            var backofficeUser = BuildBackofficeUser(request.Email);
             backofficeUserRepository
                 .Setup(x => x.SearchBy(request.Email))
                 .Returns(backofficeUser);
@@ -35,6 +39,21 @@ namespace CanaryDeliveries.Tests.Backoffice.Unit.Api.Users.Login.Services
             var areValidCredentials = loginService.AreValidCredentials(request);
 
             areValidCredentials.Should().BeTrue();
+        }
+        
+        private static LoginService.LoginRequest BuildRequest()
+        {
+            return new LoginService.LoginRequest(
+                email: "user@email.com",
+                password: "userPassword");
+        }
+
+        private static BackofficeUser BuildBackofficeUser(string userEmail)
+        {
+            return new BackofficeUser(
+                id: new Guid("9bd0af33-87b5-48d6-92a7-7122f71585a5"),
+                email: userEmail,
+                password: "hashedPassword");
         }
     }
 }
