82aca49a32fd87ed55c0872a8c541b8ccf5b0bf0
Antonio Sánchez <antoniojesussg96@gmail.com>
2020-12-13 20:39:32 +0100

    added authentication in app layer to fix jwt authetication

--- src/Backoffice/backend/Api/Startup.cs
+++ src/Backoffice/backend/Api/Startup.cs
@@ -47,6 +47,7 @@ namespace CanaryDeliveries.Backoffice.Api
             }
 
             app.UseRouting();
+            app.UseAuthentication();
             app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
