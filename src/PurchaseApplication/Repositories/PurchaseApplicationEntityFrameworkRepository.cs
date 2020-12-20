0dd6a030d28003c17fbdbb52fbd9b0274fa35b57
Antonio Sánchez <antoniojesussg96@gmail.com>
2020-12-20 22:12:08 +0100

    implemented repository metod

--- src/PurchaseApplication/Repositories/PurchaseApplicationEntityFrameworkRepository.cs
+++ src/PurchaseApplication/Repositories/PurchaseApplicationEntityFrameworkRepository.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
 using System.Linq;
 using CanaryDeliveries.PurchaseApplication.DbContext;
 using CanaryDeliveries.PurchaseApplication.Domain;
 using CanaryDeliveries.PurchaseApplication.Domain.ValueObjects;
 using LanguageExt;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic.CompilerServices;
 
 namespace CanaryDeliveries.PurchaseApplication.Repositories
 {
@@ -30,7 +33,15 @@ namespace CanaryDeliveries.PurchaseApplication.Repositories
 
         public Option<Domain.PurchaseApplication> SearchBy(Id purchaseApplicationId)
         {
-            throw new System.NotImplementedException();
+            using var dbContext = new PurchaseApplicationDbContext(purchaseApplicationDbConnectionString);
+            var id = purchaseApplicationId.State.Value;
+            return dbContext.PurchaseApplications
+                .Include(x => x.Products)
+                .Include(x => x.Client)
+                .Where(x => x.Id == id)
+                .ToList()
+                .Select(BuildPurchaseApplication)
+                .FirstOrDefault();
         }
 
         private static DbContext.PurchaseApplication BuildDbPurchaseApplication(Domain.PurchaseApplication purchaseApplication)
@@ -66,6 +77,43 @@ namespace CanaryDeliveries.PurchaseApplication.Repositories
                     Name = state.Client.Name.Value,
                     PhoneNumber = state.Client.PhoneNumber.Value
                 };
+            }
+        }
+        
+        private static Domain.PurchaseApplication BuildPurchaseApplication(DbContext.PurchaseApplication dbEntity)
+        {
+            var persistenceState = new Domain.PurchaseApplication.PersistenceStateDto(
+                id: new Id.PersistenceState(dbEntity.Id),
+                products: dbEntity.Products.Map(BuildProductPersistenceState).ToList(),
+                client: BuildClientPersistenceState(),
+                additionalInformation: dbEntity.AdditionalInformation != null
+                    ? new AdditionalInformation.PersistenceState(dbEntity.AdditionalInformation) 
+                    : Option<AdditionalInformation.PersistenceState>.None,
+                creationDateTime: dbEntity.CreationDateTime,
+                rejection: null);
+            return new Domain.PurchaseApplication(persistenceState);
+
+            Domain.Entities.Product.PersistenceState BuildProductPersistenceState(Product product)
+            {
+                return new Domain.Entities.Product.PersistenceState(
+                    id: new Id.PersistenceState(product.Id),
+                    link: new Link.PersistenceState(product.Link),
+                    units: new Units.PersistenceState(product.Units),
+                    additionalInformation: product.AdditionalInformation != null
+                        ? new AdditionalInformation.PersistenceState(product.AdditionalInformation)
+                        : Option<AdditionalInformation.PersistenceState>.None,
+                    promotionCode: product.PromotionCode != null ?
+                        new PromotionCode.PersistenceState(product.PromotionCode)
+                        : Option<PromotionCode.PersistenceState>.None);
+            }
+
+            Domain.Entities.Client.PersistenceState BuildClientPersistenceState()
+            {
+                return new Domain.Entities.Client.PersistenceState(
+                    id: new Id.PersistenceState(dbEntity.Client.Id),
+                    name: new Name.PersistenceState(dbEntity.Client.Name),
+                    phoneNumber: new PhoneNumber.PersistenceState(dbEntity.Client.PhoneNumber),
+                    email: new Email.PersistenceState(dbEntity.Client.Email));
             }
         }
     }
