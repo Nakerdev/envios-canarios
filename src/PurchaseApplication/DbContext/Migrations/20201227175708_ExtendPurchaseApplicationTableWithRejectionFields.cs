96befe9c1b0737f85708c98cdf2efb450fb37755
Antonio Sánchez <antoniojesussg96@gmail.com>
2020-12-27 18:58:12 +0100

    move connection string to dbContext

---
+++ src/PurchaseApplication/DbContext/Migrations/20201227175708_ExtendPurchaseApplicationTableWithRejectionFields.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CanaryDeliveries.PurchaseApplication.DbContext.Migrations
+{
+    public partial class ExtendPurchaseApplicationTableWithRejectionFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RejectionDateTime",
+                table: "PurchaseApplication_PurchaseApplications",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RejectionReason",
+                table: "PurchaseApplication_PurchaseApplications",
+                maxLength: 1000,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RejectionDateTime",
+                table: "PurchaseApplication_PurchaseApplications");
+
+            migrationBuilder.DropColumn(
+                name: "RejectionReason",
+                table: "PurchaseApplication_PurchaseApplications");
+        }
+    }
+}
